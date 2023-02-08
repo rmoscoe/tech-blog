e60bbc5d0849a5f12a2a7e3171e55ad5a59ed75f
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-02-07 23:05:08 -0800

    develop server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const mysql = require("mysql2/promise");
+const Sequelize = require("sequelize");
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const dotenv = require("dotenv").config();
+const bcrypt = require("bcrypt");
+const connect = require("connect-session-sequelize")(session.Store);
+ 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sess));
+
+// const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
