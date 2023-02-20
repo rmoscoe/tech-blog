75d743d36ec8f025c0e43b204063b97d0f62b17a
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-02-20 15:03:09 -0800

    build post routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const path = require("path");
+const { Post } = require(path.join(__dirname, "../../models"));
+
+// Create new post
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const post = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// Update post
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+        const { title, content } = req.body.updates;
+
+        const post = await Post.update({
+            title,
+            content
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!post[0]) {
+            res.status(404).json({ message: "Post not found." });
+            return;
+        }
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// Delete post
+    router.delete("/:id", withAuth, async (req, res) => {
+        try {
+            const post = await Post.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+
+            res.status(200).json(post);
+        } catch (error) {
+           res.status(500).json(error); 
+        }
+    });
+
+module.exports = router;
