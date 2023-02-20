ff78437276fff43fbf5ad8d71757190d50cf91f7
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-02-20 14:49:50 -0800

    build remaining user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,6 +2,28 @@ const router = require('express').Router();
 const path = require("path");
 const { User } = require(path.join(__dirname, '../../models'));
 
+// Create user record
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    const {id} = userData.get({ plain: true });
+
+    req.session.save(() => {
+      req.session.user_id = id;
+      req.session.loggedIn = true;
+
+      res.status(200).redirect("../dashboard");
+    })
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
+
+// Log a user in
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
@@ -38,6 +60,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// Log a user out
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
@@ -47,5 +70,45 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
+
+// Update a user record
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const { username, password } = req.body.updates;
+
+    const user = await User.update({
+      username,
+      password
+    }, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!user[0]) {
+      res.status(400).json({ message: "Invalid username or password." });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Delete a user record
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const user = await User.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
 
 module.exports = router;
