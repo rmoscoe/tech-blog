dffca3cf6329570aedb581e57b06e4fcdc3f5325
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-11 00:01:01 -0800

    update post form for use as create or update form

---
+++ public/js/post.js
@@ -0,0 +1,15 @@
+function editPostHandler () {
+    
+}
+
+function deletePostHandler () {
+
+}
+
+function addCommentHandler () {
+
+}
+
+document.getElementById("edit-post-button").addEventListener("click", editPostHandler);
+document.getElementById("delete-post-button").addEventListener("click", deletePostHandler);
+document.getElementById("add-comment").addEventListener("click", addCommentHandler);
